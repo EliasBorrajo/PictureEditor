87c80eda87500c435339b600bf7c28ab3dbc7e83
Elias <borrajo.elias@gmail.com>
2023-11-09 10:19:10 +0100

    refactor final architecture

---
+++ PictureEditor/BusinessLayer/Managers/ImageManager.cs
@@ -0,0 +1,44 @@
+﻿using PictureEditor.BusinessLayer.Interfaces;
+using PictureEditor.DAL;
+using System.Drawing.Imaging;
+
+namespace PictureEditor.BusinessLayer.Managers
+{
+    /// <summary>
+    /// Represents a class responsible for managing the loading and saving of images.
+    /// </summary>
+    public class ImageManager : IImageManager
+    {
+        /// <summary>
+        /// Loads an image using the provided IOutputInput instance and file name.
+        /// </summary>
+        /// <param name="outputInput">The IOutputInput instance used for loading the image.</param>
+        /// <param name="name">The name or path of the file to be loaded.</param>
+        /// <returns>The loaded Image object.</returns>
+        public Image Load(IOutputInput outputInput, string name)
+        {
+            return outputInput.Load(name);
+        }
+
+        /// <summary>
+        /// Saves the provided image using the specified IOutputInput instance, file name, and image format.
+        /// </summary>
+        /// <param name="outputInput">The IOutputInput instance used for saving the image.</param>
+        /// <param name="imageToSave">The image to be saved.</param>
+        /// <param name="name">The name or path of the file to save.</param>
+        /// <param name="format">The format in which the image should be saved.</param>
+        /// <returns>True if the image is saved successfully; otherwise, false.</returns>
+        public bool Save(IOutputInput outputInput, Image imageToSave, string name, ImageFormat format)
+        {
+            try
+            {
+                outputInput.Save(imageToSave, name, format);
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+    }
+}
